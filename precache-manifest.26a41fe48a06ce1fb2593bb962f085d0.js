05ede4f952d6dee3ecdba5eb07060caaafdf18c7
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-01-08 22:40:32 +0100

    Updates

---
+++ precache-manifest.26a41fe48a06ce1fb2593bb962f085d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5694f1d7d596825f9d43d97895152a62",
+    "url": "/Todo-List-React-/index.html"
+  },
+  {
+    "revision": "f8b89d8503693d23afc2",
+    "url": "/Todo-List-React-/static/css/main.3cd6de63.chunk.css"
+  },
+  {
+    "revision": "9b615fd6d21a02ebeb71",
+    "url": "/Todo-List-React-/static/js/2.5aa2e8f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-List-React-/static/js/2.5aa2e8f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8b89d8503693d23afc2",
+    "url": "/Todo-List-React-/static/js/main.427688c9.chunk.js"
+  },
+  {
+    "revision": "60962e5a8cc4a921febb",
+    "url": "/Todo-List-React-/static/js/runtime-main.9f928eaa.js"
+  }
+]);
